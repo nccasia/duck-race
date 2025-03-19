b625b396bacaaa48a7ff969da624a7208310d08f
Nguyen Ngoc Anh Tuan <Tomtepfa@gmail.com>
2025-03-19 14:52:13 +0700

    fix(cors): update CORS origin URL for production environment

--- duckrace-be/src/configs/corsConfig.ts
+++ duckrace-be/src/configs/corsConfig.ts
@@ -2,7 +2,7 @@ export const corsConfig = {
   origin: [
     "http://localhost:5173",
     "https://admin.socket.io",
-    "https://duckrace.vncsoft.com",
+    "https://duck-race.nccsoft.vn",
   ],
   credentials: true,
 };
