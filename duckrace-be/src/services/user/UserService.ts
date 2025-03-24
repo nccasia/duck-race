579ba0ededaea99700742f2cc6f90db4e7d90d37
Nguyen Ta Quyen <ta2k3quyen@gmail.com>
2025-03-24 21:35:46 +0700

    fix hash login

--- duckrace-be/src/services/user/UserService.ts
+++ duckrace-be/src/services/user/UserService.ts
@@ -29,7 +29,7 @@ class UserService implements IUserService {
 
       const { hash, ...hashParams } = mezonEventData as HashData;
       const mezonUser = JSON.parse(hashParams?.user) as MezonUser;
-      const hashParamsString = queryString.stringify(hashParams, { sort: false });
+      const hashParamsString = rawHashData.split("&hash=")[0];
 
       const botToken = process.env.MEZON_APP_TOKEN || "";
       const secretKey = Hasher.HMAC_SHA256(botToken, "WebAppData");
